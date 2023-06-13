1aaf8eddc6a2a1e90f0688bf6691c5dc34eb53cc
Serhii Klokun <klokuns@gmail.com>
2023-06-13 19:28:51 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
-// import { lazy } from 'react';
+import { lazy } from 'react';
 
-import Home from '../page/Home/Home';
-import Movies from '../page/Movies';
-import MovieDetails from '../page/MovieDetails/MovieDetails';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews';
+// import Home from '../page/Home/Home';
+// import Movies from '../page/Movies';
+// import MovieDetails from '../page/MovieDetails/MovieDetails';
+// import Cast from './Cast/Cast';
+// import Reviews from './Reviews';
 
-
-// const Home = lazy(() => import('../page/Home/Home.jsx'));
-// const Movies = lazy(() => import('../page/Movies.jsx'));
-// const MovieDetails = lazy(() => import('../page/MovieDetails/MovieDetails.jsx'));
-// const Cast = lazy(() => import('./Cast/Cast.jsx'));
-// const Reviews = lazy(() => import('./Reviews.jsx'));
-
-
-
+const Home = lazy(() => import('../page/Home/Home.jsx'));
+const Movies = lazy(() => import('../page/Movies.jsx'));
+const MovieDetails = lazy(() =>
+  import('../page/MovieDetails/MovieDetails.jsx')
+);
+const Cast = lazy(() => import('./Cast/Cast.jsx'));
+const Reviews = lazy(() => import('./Reviews.jsx'));
 
 export const App = () => {
   return (
