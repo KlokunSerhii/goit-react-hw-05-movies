8007dff59259a74fbd13034ef46daf1ac800d485
Serhii Klokun <klokuns@gmail.com>
2023-06-13 19:43:42 +0300

    fix router-dom

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,10 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Loader from 'components/Loader';
 import { Outlet } from 'react-router-dom';
 import { StyledLink, Svg, Main } from './SharedLayout.styled';
 import { Suspense } from 'react';
-import Loader from 'components/Loader';
 
 export const SharedLayout = () => {
   return (
